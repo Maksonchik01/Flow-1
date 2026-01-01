48cbe59696c58cba63e6020e837185280c06a6eb
Maksym_Demydasiuk <maksdem1406@gmail.com>
2026-01-01 23:56:47 +0100

    .

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const burger = document.querySelector(".header-burger-menu");
+
+
+const burgerBg = document.querySelector(".header-burger-menu-bg");
+const close = document.querySelector(".header-burger-menu-close-menu");
+
+function openBurger() {
+  burgerBg.classList.add("active")
+}
+burger.addEventListener("click", openBurger)
+
+function closeBurger() {
+  burgerBg.classList.remove("active")
+}
+close.addEventListener("click", closeBurger)
+
 const scrollContainer = document.querySelector('.reviews-wrapper-scroll');
 const progressFill = document.querySelector('.reviews-wrapper-scroll-line-fill');
 
